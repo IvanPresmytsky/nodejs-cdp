f54e90a50dec899dadaae4d9ba1baa3b9d73844c
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2018-05-27 22:43:22 +0300

    implement transform function

--- actions/transform.js
+++ actions/transform.js
@@ -1,5 +1,20 @@
-function transform(str) {
-  console.log('transforming... ' + str);
+const through = require('through2');
+const stream = through(write, end);
+
+function write (buffer, encoding, next) {
+  const transformedData = buffer.toString().toUpperCase();
+  this.push(transformedData + '\n');
+  next();
+}
+
+function end (done) {
+  console.log('Finis transforming...');
+  done();
+}
+
+function transform() {
+  console.log('transforming data to upper case...');
+  process.stdin.pipe(stream).pipe(process.stdout);
 }
 
 module.exports = transform;
