32bb18433525338cdf6947cec811401f314bcb56
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2018-05-28 00:06:04 +0300

    implement outputFile logic

--- actions/outputFile.js
+++ actions/outputFile.js
@@ -1,5 +1,12 @@
+const fs = require('fs');
+
 function outputFile(filePath) {
-  console.log('output file... ' + filePath);
+  console.log(`reading data from file ${filePath} ...`);
+  fs.createReadStream(filePath)
+  .on('error', function(error) {
+    console.error(error);
+  })
+  .pipe(process.stdout);
 }
 
 module.exports = outputFile;
