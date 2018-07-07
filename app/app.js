501e09bd5af3259e70bf0ea1e8b40cf2465e2e4e
Ivan Prasmytski <ivan_prasmytski@epam.com>
2018-07-07 16:14:20 +0300

    modify app.js

--- app/app.js
+++ app/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const passport = require('passport');
 const routes = require('./routes/appRoutes');
+const session = require('express-session');
 const { cookieParser, qsParser } = require('./middlewares');
 
 const app = express();
 const bodyParser = express.json();
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 app.use(bodyParser);
 
@@ -11,8 +19,13 @@ app.use(cookieParser());
 
 app.use(qsParser());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 routes(app);
 
-//app.use('/', router);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 module.exports = app;
